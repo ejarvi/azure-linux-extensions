b17f602aad92137003283eb8a2b1cb5ea8bb2d89
Kamran Khan <kakhan@microsoft.com>
2017-02-10 13:18:57 -0800

    Update copyright notices

--- VMEncryption/main/KeyVaultUtil.py
+++ VMEncryption/main/KeyVaultUtil.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# VM Backup extension
+# Azure Disk Encryption For Linux extension
 #
-# Copyright 2015 Microsoft Corporation
+# Copyright 2016 Microsoft Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
